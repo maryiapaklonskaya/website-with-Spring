6c4cb887c7d0b01605f08fa710c9345b2ab18da1
mpaklonskaya <mariapoklonskaya@gmail.com>
2023-01-25 18:32:21 +0200

    cmmit

---
+++ src/main/java/com/websitewithspring/WebsiteWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.websitewithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websitewithspring/WebsiteWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websitewithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
